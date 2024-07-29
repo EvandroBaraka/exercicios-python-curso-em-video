f0ee9ea7ab3cd0da4bcd4465c2ce30c9b3383132
EvandroBaraka <evandrobaraka@gmail.com>
2024-07-29 15:33:08 -0300

    enviando um arquivo para teste de repositório

--- ex087.py
+++ ex087.py
@@ -13,7 +13,7 @@ for l in range(0, 3):
 	for c in range(0, 3):
 		print(f'[ {matriz[l][c]} ]', end='' )
 	print('\n')
-print('-=-' * 15)
+print('=-=' * 15)
 print(f'A soma dos números pares é {pares}')
 print(f'A soma dos valores da terceira coluna é {coluna3}')
 print(f'O maior valor da segunda linha é {max(matriz[1])}')
